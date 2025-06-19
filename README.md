7a3d340e427be8d4083572f2f0e0672c26c360e2
ShizerStorage <kranpetrov88@gmail.com>
2025-06-19 12:28:33 +0300

    Upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## Feedback
+
+![image](https://github.com/user-attachments/assets/385b4e12-b225-49f5-96bc-f94a5d58c086)
+
+![image](https://github.com/user-attachments/assets/10cdb3c1-f1f4-4af5-8301-6c4a0b988081)
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="p-4 text-center border-t">
+      <p className="text-gray-500">2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
